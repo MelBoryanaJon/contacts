f02294dba8094fc51937d43744ee865c8fc9044f
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-01 12:01:46 -0500

    contacts in progress

--- contacts/Main.java
+++ contacts/Main.java
@@ -13,12 +13,13 @@ public class Main {
         startPhone();
         printAction();
         while (!quite) {
-            System.out.println("\n Enter action: (6 to show available actions)");
+            System.out.println("Enter an option (1, 2, 3, 4 or 5)");
             int action = scanner.nextInt();
             scanner.nextLine();
 
             switch (action) {
                 case 0:
+
                     System.out.println("\n Exit");
                     quite = true;
                     break;
@@ -120,13 +121,12 @@ public class Main {
     }
     private static void printAction() {
         System.out.println("\nAvailable actions:\npress");
-        System.out.println("0 - to shutDown\n" +
-                "1 - to print contacts\n"+
-                "2 - to add a new contact\n"+
-                "3 - to update an existing contact\n"+
-                "4 - to remove and existing contact\n"+
-                "5 - query for existing contact\n"+
-                "6 - to print a list of available actions.");
+        System.out.println(
+                "1. View Contacts\n"+
+                "2. Add a new contact\n"+
+                "3. Search a contact by name\n"+
+                "4. Delete an existing contact.\n"+
+                "5. Exit\n");
         System.out.println("Choose Your Action: ");
     }
 
